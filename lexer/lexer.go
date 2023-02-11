219ee549e5fe41f9f5b7bd64518bacb0b05fcc70
ittyi <vioiv.k4@gmail.com>
2023-02-11 14:36:10 +0900

    🚧 雛形作成用のコード保存。interface、import周りを要調査

---
+++ lexer/lexer.go
@@ -0,0 +1,13 @@
+package lexer
+
+// "github.com/product-dev-group/gosh/blob/main/interface/shellInterface"// できないの調査
+
+// class Lexer implements Shell {
+// 	execute() {
+//
+// }
+// }
+
+// func (test *shellInterface.Shell) lexer() {
+
+// }
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "log"
+import (
+	"log"
+	// "github.com/product-dev-group/gosh/blob/main/interface/shellInterface" // できないの要調査
+)
 
 func main() {
 	log.Printf("Hello, World")
+	// log.Printf("test: %#v\n", shellInterface.Shell)
 }
