650e278f239fd78e53ffdcd3a73b3d6171765629
ittyi <vioiv.k4@gmail.com>
2023-02-11 14:12:38 +0900

    🏷️ インターフェースのガワだけ準備

---
+++ interface/shell.go
@@ -0,0 +1,15 @@
+package shellInterface
+
+type Shell interface {
+	Execute()
+}
+
+// type Hoge interface {
+// 	FuncA()
+//         FuncB()
+// }
+
+// type Foo struct {}
+
+// func (f *Foo) FuncA() {}
+// func (f *Foo) FuncB() {}
---
+++ lexer.go
@@ -0,0 +1,15 @@
+package lexer
+
+import (
+	"./interface/shellInterface"
+)
+
+// class Lexer implements Shell {
+// 	execute() {
+//
+// }
+// }
+
+func (test *shellInterface.Shell) lexer() {
+
+}
